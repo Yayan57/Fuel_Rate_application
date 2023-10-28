9d856e4c1ef842f9c1ed3b5be7eed97fb5e87b5d
CitadelCTDL <116697752+CitadelCTDL@users.noreply.github.com>
2023-10-27 20:55:53 -0500

    Update Fuel_Quote_Form.js
    Added insert query to create new data for order submissions

--- Fuel_Quote_Form.js
+++ Fuel_Quote_Form.js
@@ -3,10 +3,27 @@ const app = express()
 const port = 3000;
 
 app.use(express.static('public'))
-
 var urlencodedParser = bodyParser.urlencoded({ extended: true })
 
+var mysql = require('mysql');
+var db = mysql.createConnection({
+  host: 'localhost', // Replace with host name
+  user: 'root',      // Replace with database username
+  password: '',      // Replace with database password
+  database: '' // // Replace with database Name
+}); 
+conn.connect(function(err) {
+  if (err) throw err;
+  console.log('Database is connected successfully !');
+});
+module.exports = db;
+
 const orders = [];
+
+app.get('*', function(req, res, next){
+    res.locals.user = req.user || null;
+    next();
+})
 
 app.get('/', function (req, res) {
     res.sendFile("./Fuel_Quote_Form.html");
@@ -17,23 +34,20 @@ app.listen(port, function() {
 }); 
 
 app.post('/orders', urlencodedParser, function (req, res) {
+    var userid = req.user.username
     var gallons = req.body.gallons_requested;
     var address = req.body.delivery_address;
     var date = req.body.delivery_date;
     var ppg = req.body.price_per_gallon;
     var total = req.body.total_amount;
-    if (!gallons || !address || !date || !ppg || !total) {
+    if (!username || !gallons || !address || !date || !ppg || !total) {
       res.status(400).json({ message: 'Incomplete order request' });
     } else {
-      const newOrder = {
-        id: orders.length + 1,
-        gallons,
-        address,
-        date,
-        ppg,
-        total,
-      };
-      orders.push(newOrder);
+        db.query('INSERT INTO FuelQuote (userID, gallonsRequested, deliveryAddress, deliveryDate, ppg, totalPrice) VALUES (?, ?, ?, ?, ?, ?)', [userid, gallons, address, date, ppg, total], (err, result) => {
+        if (err) {
+            return res.status(500).json({ message: 'Database error' });
+    }
+      //orders.push(newOrder);
       res.status(201).json(newOrder);
     }
   });
