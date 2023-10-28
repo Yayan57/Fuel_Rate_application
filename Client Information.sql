eaa491e9d0f2beb2891fb02e7593bba9b8305226
CitadelCTDL <116697752+CitadelCTDL@users.noreply.github.com>
2023-10-27 19:18:02 -0500

    Update and rename Client Information.sql to Fuel_Rate_Application.sql
    Added the fuel quote table

--- Client Information.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE client_profiles;
-USE client_profiles;
-CREATE TABLE clients (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  address VARCHAR(255) NOT NULL
-);
---
+++ Fuel_Rate_Application.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE fuel_rate_application;
+USE fuel_rate_application;
+
+CREATE TABLE clients (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  address VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE FuelQuote (
+    orderID INT PRIMARY KEY,
+    userID INT NOT NULL,
+    gallonsRequested INT NOT NULL,
+    deliveryAddress VARCHAR(255) NOT NULL,
+    deliveryDate DATE NOT NULL,
+    ppg FLOAT NOT NULL,
+    totalPrice FLOAT NOT NULL
+);
