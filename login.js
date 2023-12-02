0fec68760714b00dc6a00ef8b1f67be6d98523d9
dtranow <104113410+dtranow@users.noreply.github.com>
2023-12-01 23:53:47 -0600

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,16 @@ const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
 const mysql = require("mysql");
+const session = require('express-session');
+const crypto = require('crypto');
 
+const secret = crypto.randomBytes(32).toString('hex');
+
+app.use(session({
+  secret: secret,
+  resave: true,
+  saveUninitialized: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -66,6 +75,8 @@ if(username && password){
         const match = bcrypt.compareSync(password, user.password);
         
         if(match){
+          request.session.loggedin = true;
+          request.session.username = username;
           response.redirect('/Fuel_Quote_Form');
         }
         else{
@@ -81,6 +92,28 @@ if(username && password){
 else{
   response.status(401).send('Login Failed');
 }
+});
+
+function requireLogin(request, response, next) {
+  if(request.session.loggedin){
+    next();
+  }else
+  {
+    response.redirect('/login');
+  }
+}
+
+app.get('/Fuel_Quote_Form', requireLogin, (request, response) => {
+  response.render('Fuel_Quote_Form', { username: request.session.username });
+});
+
+app.get('/logout', (request, response) => {
+  request.session.destroy((err) => {
+    if(err){
+      console.error(err);
+    }
+    response.redirect('/login');
+  });
 });
 
 app.listen(3000);
